56d4630389fbd9a4502df91857142afec3d1e8ad
Sampo Harjula <Sampo.Harjula@student.lamk.fi>
2017-02-15 12:04:20 +0200

    Bootstrap, JQuery...

--- veppilaskuri/Default.aspx.cs
+++ veppilaskuri/Default.aspx.cs
@@ -9,6 +9,11 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        /* https://stackoverflow.com/questions/20151556/how-to-get-the-http-post-data-in-c */
+        string[] keys = Request.Form.AllKeys;
+        for (int i = 0; i < keys.Length; i++)
+        {
+            Response.Write(keys[i] + ": " + Request.Form[keys[i]] + "<br>");
+        }
     }
 }
